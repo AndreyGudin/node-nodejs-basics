38a7024185b05d7f68b80420189f4e951b4e6e91
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 16:18:45 +0400

    feat: cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import { env } from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const envs = env;
+  for (let key in envs) {
+    if (key.split("_")[0] === "RSS") console.log(`${key}=${envs[key]}`);
+  }
 };
 
 parseEnv();
