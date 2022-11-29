12a8826cc5ad51f029068691897436dae2fbfec2
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 14:57:49 +0400

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from "node:fs/promises";
+import { URL } from "url";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const FOLDER = new URL("files", import.meta.url);
+    const files = await readdir(FOLDER);
+    for (const file of files) console.log(file);
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await list();
