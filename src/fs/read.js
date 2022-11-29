a6d6225d1d45178ea3fcb33c70218205bb5f0b12
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 15:44:21 +0400

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { open } from "node:fs/promises";
+import { URL } from "url";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const FILE = new URL("files/fileToRead.txt", import.meta.url);
+    const file = await open(FILE);
+    let data = "";
+    const stream = file.createReadStream();
+    stream.on("data", (chunk) => (data += chunk));
+    stream.on("end", () => console.log(data));
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await read();
