f0c23e577a8f5515d00ae6d928947d0e0c793969
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 14:45:07 +0400

    feat: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import { URL } from "url";
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const FOLDER_COPY_FROM = new URL("files", import.meta.url);
+    const FOLDER_COPY_TO = new URL("files_copy", import.meta.url);
+    await cp(FOLDER_COPY_FROM, FOLDER_COPY_TO, { recursive: true, force: false, errorOnExist: true });
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,14 +1,14 @@
-import { writeFile } from 'node:fs/promises';
-import { URL } from 'url';
+import { writeFile } from "node:fs/promises";
+import { URL } from "url";
 
 const create = async () => {
-    try {
-        const FOLDER = new URL('files/fresh.txt',import.meta.url) ;
-        const content = 'I am fresh and young';
-        await writeFile(FOLDER,content,{flag: 'wx'});
-    } catch (error) {
-        console.log('FS operation failed');
-    }
+  try {
+    const FOLDER = new URL("files/fresh.txt", import.meta.url);
+    const content = "I am fresh and young";
+    await writeFile(FOLDER, content, { flag: "wx" });
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await create();
