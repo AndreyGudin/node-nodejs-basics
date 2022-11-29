00b0477496a35052998772c23892c9209962d192
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 14:51:47 +0400

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { URL } from "url";
+import { rename as renameFile } from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const wrongFilename = new URL("files/wrongFilename.txt", import.meta.url);
+    const properFilename = new URL("files/properFilename.txt", import.meta.url);
+    await renameFile(wrongFilename, properFilename);
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await rename();
