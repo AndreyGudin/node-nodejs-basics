a17e20e800242424075ca90d7d1be09e505e3666
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 14:19:25 +0400

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from 'node:fs/promises';
+import { URL } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const FOLDER = new URL('files/fresh.txt',import.meta.url) ;
+        const content = 'I am fresh and young';
+        await writeFile(FOLDER,content,{flag: 'wx'});
+    } catch (error) {
+        console.log('FS operation failed');
+    }
 };
 
 await create();
