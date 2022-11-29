2e3ae60efb86ca303dae0a9c8e48c9131a2462a3
Andrey_Gudin <aeon150791@gmail.com>
2022-11-29 14:54:03 +0400

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { rm } from "node:fs/promises";
+import { URL } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const FILE_TO_DELETE = new URL("files/fileToRemove.txt", import.meta.url);
+    await rm(FILE_TO_DELETE);
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await remove();
