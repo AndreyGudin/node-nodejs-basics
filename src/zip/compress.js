013d26aa88e92609396e8cd2f90abba15ea58e90
Andrey_Gudin <aeon150791@gmail.com>
2022-11-30 11:47:43 +0400

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createReadStream,createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { URL } from "url";
+
 const compress = async () => {
-    // Write your code here 
+    const FILE = new URL("files/fileToCompress.txt", import.meta.url);
+    const ARCHIVE = new URL("files/archive.gz", import.meta.url);
+    const gzip = createGzip();
+    const rstream = createReadStream(FILE);
+    const wstream = createWriteStream(ARCHIVE);
+    rstream.pipe(gzip).pipe(wstream);
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream,createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { URL } from "url";
+
 const decompress = async () => {
-    // Write your code here 
+    const FILE = new URL("files/fileToCompress.txt", import.meta.url);
+    const ARCHIVE = new URL("files/archive.gz", import.meta.url);
+    const ungzip = createGunzip();
+    const rstream = createReadStream(ARCHIVE);
+    const wstream = createWriteStream(FILE);
+    rstream.pipe(ungzip).pipe(wstream); 
 };
 
 await decompress();
