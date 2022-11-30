d1cdd7ae68c4d270fc1c600be4ef0dfa9bf58ad8
Andrey_Gudin <aeon150791@gmail.com>
2022-11-30 10:18:17 +0400

    featL readstream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from "node:fs";
+import { URL } from "url";
+import { stdout } from "process";
+
 const read = async () => {
-    // Write your code here 
+    const FILE = new URL("files/fileToRead.txt", import.meta.url);
+    const stream = createReadStream(FILE);
+    let data = "";
+    stream.on("data", (chunk) => (data += chunk));
+    stream.on("end", () => stdout.write(data));
 };
 
 await read();
