c8e321f897f5a6b6a004d8bb90d789f9ac93da6d
Andrey_Gudin <aeon150791@gmail.com>
2022-11-30 10:12:27 +0400

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const FILE = new URL("files/fileToCalculateHashFor.txt", import.meta.url);
+  const stream = createReadStream(FILE);
+  const hash = createHash("sha256");
+  stream.on("readable", () => {
+    const data = stream.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
