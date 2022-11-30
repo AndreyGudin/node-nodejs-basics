48a0ffcf8d9a354ddd9cbe2957f3d2c3e5adf5c1
Andrey_Gudin <aeon150791@gmail.com>
2022-11-30 11:26:01 +0400

    feat: transfrom

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "node:fs";
+import { Transform } from "node:stream";
+import { URL } from "url";
+import { stdin,stdout } from "process";
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback){
+            callback(null, chunk.toString().split("").reverse().join(""));
+        }
+    });
+    stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
