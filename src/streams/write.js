5fb1dbc906f70985a534f496eb69e8234202399d
Andrey_Gudin <aeon150791@gmail.com>
2022-11-30 10:28:24 +0400

    feat: writesreams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from "node:fs";
+import { URL } from "url";
+import { stdin } from "process";
+
 const write = async () => {
-    // Write your code here 
+  const FILE = new URL("files/fileToWrite.txt", import.meta.url);
+  const stream = createWriteStream(FILE);
+  stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
 
 await write();
